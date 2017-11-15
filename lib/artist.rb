ad2b6278c9fc33df7e150c44901451c7e4c4bc13
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-11-15 16:56:43 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,43 @@
+class Artist
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    self.save
+  end
+
+  def add_song(song)
+      @songs << song
+      song.artist=(self)
+  end
+
+  def songs
+    @songs
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    if !(@@all.detect {|artist| artist.name == artist_name})
+        artist = self.new(artist_name)
+        #how to add that song
+        #artist.add_song()
+
+      end
+  end
+
+  def print_songs
+    self.songs.collect do |song|
+      puts song.name.to_s
+    end
+  end
+
+end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,20 @@
+
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.entries(self.path).select{ |f| f.match(/(\.mp3)$/) }
+
+  end
+
+  def import
+    if !self.files.nil?
+      self.files.each {|filename| Song.new_by_filename(filename)}
+    end
+    (Artist.all.first.songs.empty?) && (Artist.all.last.songs.empty?)
+  end
+end

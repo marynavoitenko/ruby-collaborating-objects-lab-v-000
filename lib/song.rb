33141b8b641087fe49476792805e41c9dc16683d
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-11-15 19:28:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,8 +5,14 @@ class Song
     @name = name
   end
 
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
   def artist_name=(artist_name)
       self.artist = Artist.find_or_create_by_name(artist_name)
+      
   end
 
   def self.new_by_filename(filename)
